74714ef391c0c75ec8fdef0c74b9c082a52a1175
Stephen Morse <stephen.morse@revature.net>
2021-03-24 10:09:42 -0700

    First Commit for Project 0

---
+++ src/App.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class App {
+    private static String filename = "IngredientList.txt";
+    public static void main(String[] args) {
+        try {
+            System.out.println("Opening Input File...");
+            File inputFile = new File(filename);
+            Scanner fileReader = new Scanner(inputFile);
+            System.out.println("Reader Opened Successfully!");
+            //Read File Contents
+            while (fileReader.hasNextLine()) {
+                String inputLine = fileReader.nextLine();
+                System.out.println(inputLine);
+            }
+            fileReader.close();
+            System.out.println("Reader Closed Successfully!");
+        } catch (FileNotFoundException ep) {
+            System.out.println("FileNotFoundException occurred:");
+            ep.printStackTrace();
+        }
+    }
+}
